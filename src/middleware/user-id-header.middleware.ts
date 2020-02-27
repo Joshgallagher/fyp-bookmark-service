5711c0854356377f9825310f4e20aef9271b3d40
Josh <joshgallagher24@gmail.com>
2020-02-27 20:42:32 +0000

    Added exception checking

--- src/middleware/user-id-header.middleware.ts
+++ src/middleware/user-id-header.middleware.ts
@@ -1,6 +1,7 @@
 import { Context } from 'mali';
 import bearer from '@malijs/bearer';
-import { JWT } from 'jose';
+import { JWT, errors } from 'jose';
+import RpcException from 'grpc-error';
 import { status } from 'grpc';
 
 const OPTIONS = { error: { message: 'NOT_AUTHORISED', code: status.PERMISSION_DENIED } };
@@ -14,11 +15,26 @@ export const userIdHeaderMiddleware = (): Function => {
         OPTIONS,
         async (token: string, context: Context, next: Function): Promise<void> => {
             try {
-                const { sub } = JWT.decode(token) as Record<any, any>;
+                const { sub } = JWT.decode(token) as Record<string, any>;
 
                 context.req.subject = sub;
             } catch (e) {
-                //
+                const error = e as any;
+                const code = e.code.substring(4);
+
+                if (
+                    (e instanceof errors.JOSEError && error.code === 'ERR_JWT_CLAIM_INVALID') ||
+                    (e instanceof errors.JOSEError && error.code === 'ERR_JWT_EXPIRED') ||
+                    (e instanceof errors.JOSEError && error.code === 'ERR_JWT_MALFORMED')
+                ) {
+                    throw new RpcException(code, status.FAILED_PRECONDITION, {
+                        error: e.message,
+                    });
+                }
+
+                throw new RpcException(code, status.INTERNAL, {
+                    error: e.message,
+                });
             }
 
             await next();
