d6b4575ffc3431232a5bb6a79a6be87d6e67ec94
Joshua Gallagher <joshgallagher24@gmail.com>
2020-04-26 14:34:16 +0100

    Added integration tests around the bookmark RPC endpoints (#3)

--- __tests__/helper/rpc-client.helper.ts
+++ __tests__/helper/rpc-client.helper.ts
@@ -1,8 +1,8 @@
 import caller from 'grpc-caller';
 import { resolve } from 'path';
 
-const PROTO_PATH = resolve(__dirname, '../../src/proto/user.proto');
-const PROTO_SERVICE = 'UserService';
+const PROTO_PATH = resolve(__dirname, '../../src/proto/bookmarks.proto');
+const PROTO_SERVICE = 'BookmarksService';
 
 export const rpcClient = async (serverPort: number) => {
     return await caller(`${process.env.HOST}:${serverPort}`, PROTO_PATH, PROTO_SERVICE);
